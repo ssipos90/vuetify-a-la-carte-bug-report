6297d37ef625e84705791ddfe5157b04e1ff7d79
Sebastian Sipos <ssipos@pentalog.com>
2018-01-17 18:14:55 +0200

    Reproduce error.

---
+++ src/SomeComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-content v-for="(item,i) in 5" :key="i">
+      <div slot="header">Item</div>
+      <p>{{ item }}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+<script>
+  import { VExpansionPanel } from 'vuetify';
+  export default {
+    components: {
+      VExpansionPanel
+    }
+  };
+</script>
